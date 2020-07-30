c697d3c49a9ab591a2c59cc47f9d8901fec0b432
Kiersty Shoultz <kierstycaekz@live.com>
2020-07-30 17:42:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |num|
-    num_squared = num * num
-    square_array << num_squared
-    
+    new_array << num ** 2
+    new_array
   end
   
